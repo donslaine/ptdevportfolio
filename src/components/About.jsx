fc637658fbd06864173b6d183042e77e3b11bf78
donslaine <101227886+donslaine@users.noreply.github.com>
2023-03-18 13:41:11 -0400

    removes about me image border

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,14 +13,14 @@ const AboutMe = () => {
 
         <div className="grid-cols-12 items-center md:grid">
           <div className="col-span-4 sm:text-center md:text-left">
-            <div className="relative inline-block leading-none drop-shadow-thumb sm:before:absolute sm:before:bottom-[-20px] sm:before:right-[-25px] sm:before:h-full sm:before:w-[calc(100%-20px)] sm:before:rounded-br-3xl sm:before:border-2 sm:before:border-white-light md:block lg:before:bottom-[-45px]">
-              <img
-                width={380}
-                height={470}
-                className="rounded-md"
-                src={`/images/${about.thumb}`}
-              />
-            </div>
+            {/* <div className="relative inline-block leading-none drop-shadow-thumb sm:before:absolute sm:before:bottom-[-20px] sm:before:right-[-25px] sm:before:h-full sm:before:w-[calc(100%-20px)] sm:before:rounded-br-3xl sm:before:border-2 sm:before:border-white-light md:block lg:before:bottom-[-45px]"> */}
+            <img
+              width={380}
+              height={470}
+              className="rounded-md drop-shadow-thumb"
+              src={`/images/${about.thumb}`}
+            />
+            {/* </div> */}
           </div>
 
           <div className="col-start-6 col-end-13">
